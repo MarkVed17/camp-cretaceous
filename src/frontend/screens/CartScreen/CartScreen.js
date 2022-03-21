bd161f2256f5753f218f6429f4ce3a2d5a2c250a
MarkVed17 <vedantlahane17@gmail.com>
2022-03-22 03:13:11 +0530

    cart screen placeholder

---
+++ src/frontend/screens/CartScreen/CartScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CartScreen() {
+  return (
+    <div
+      className="main-content"
+      style={{ color: "white", marginTop: "10rem", textAlign: "center" }}
+    >
+      Cart Screen
+    </div>
+  );
+}
+
+export { CartScreen };
