86e76c79f3a3f9b36ae44962ff16b93ca2145e81
MarkVed17 <vedantlahane17@gmail.com>
2022-03-23 00:22:45 +0530

    created & consumed products context

---
+++ src/frontend/contexts/products-context.js
@@ -0,0 +1,36 @@
+import { useState, useEffect, createContext, useContext } from "react";
+import axios from "axios";
+const ProductsContext = createContext();
+
+const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get("/api/products");
+        setProducts(data.products);
+      } catch (err) {
+        console.error(err.message);
+      }
+    })();
+  }, []);
+
+  return (
+    <ProductsContext.Provider value={{ products }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+
+const useProducts = () => {
+  const context = useContext(ProductsContext);
+
+  if (context === undefined) {
+    throw new Error("useProducts must be used within a ProductsProvider");
+  }
+
+  return context;
+};
+
+export { useProducts, ProductsProvider };
