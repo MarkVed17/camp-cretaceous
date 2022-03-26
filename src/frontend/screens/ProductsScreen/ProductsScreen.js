f17e620e2ff8d5174f679eb8247e20ffec056104
MarkVed17 <vedantlahane17@gmail.com>
2022-03-26 15:26:26 +0530

    updated Screen & Components wrt context

--- src/frontend/screens/ProductsScreen/ProductsScreen.js
+++ src/frontend/screens/ProductsScreen/ProductsScreen.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Filters, ProductsListing } from "../../components";
+import { useProducts, useFilters } from "../../contexts";
+import { getFinalProducts } from "../../utils";
 import "./ProductsScreen.css";
 
 function ProductsScreen() {
+  const { products } = useProducts();
+
+  const { filters, dispatchFilters } = useFilters();
+  const { sortBy, categories, rating, maxPrice } = filters;
+
+  const finalProducts = getFinalProducts(products, filters);
+
   return (
     <div className="main-container">
-      <Filters />
-      <ProductsListing />
+      <Filters
+        filters={{ sortBy, categories, rating, maxPrice }}
+        dispatchFilters={dispatchFilters}
+      />
+      <ProductsListing finalProducts={finalProducts} />
     </div>
   );
 }
