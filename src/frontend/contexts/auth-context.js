166c1e4cbe205370310b1c9bb84ab7e31091a963
MarkVed17 <vedantlahane17@gmail.com>
2022-03-28 16:36:39 +0530

    created & consumed auth-context

---
+++ src/frontend/contexts/auth-context.js
@@ -0,0 +1,29 @@
+import { useState, createContext, useContext } from "react";
+import { AUTH_TOKEN } from "./../constants/authConstants";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({
+    status: localStorage.getItem(AUTH_TOKEN) ? true : false,
+    token: localStorage.getItem(AUTH_TOKEN),
+  });
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (context === undefined) {
+    throw new Error("useAuth must be used within a AuthProvider");
+  }
+
+  return context;
+};
+
+export { useAuth, AuthProvider };
