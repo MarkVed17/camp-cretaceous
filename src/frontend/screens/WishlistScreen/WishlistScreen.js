6924abcb1115f4a641177ac5235eee011416821d
MarkVed17 <vedantlahane17@gmail.com>
2022-03-29 17:07:53 +0530

    feat: added Wishlist screen

--- src/frontend/screens/WishlistScreen/WishlistScreen.js
+++ src/frontend/screens/WishlistScreen/WishlistScreen.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { WishlistProductCard } from "../../components";
+import { useWishlist } from "../../contexts";
+import "./WishlistScreen.css";
 
 function WishlistScreen() {
+  const { wishlist } = useWishlist();
+
   return (
-    <div
-      className="main-content"
-      style={{ color: "white", marginTop: "10rem", textAlign: "center" }}
-    >
-      Wishlist Screen
+    <div className="wishlist-container">
+      {/* <!-- Wishlist Heading --> */}
+      <h1 className="wishlist-heading">Wishlist</h1>
+
+      {/* <!-- Wishlist Items --> */}
+      {wishlist.map((product) => (
+        <WishlistProductCard key={product._id} product={product} />
+      ))}
     </div>
   );
 }
