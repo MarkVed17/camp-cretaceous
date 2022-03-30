e3337c0c2d9dc8f2219fb12e810d1790c9a8c775
MarkVed17 <vedantlahane17@gmail.com>
2022-03-30 11:39:23 +0530

    feat: added & consumed cart-context

--- src/frontend/contexts/index.js
+++ src/frontend/contexts/index.js
@@ -6,3 +6,4 @@ export {
 } from "./filters-context";
 export { useAuth, AuthProvider } from "./auth-context";
 export { useWishlist, WishlistProvider } from "./wishlist-context";
+export { useCart, CartProvider } from "./cart-context";
