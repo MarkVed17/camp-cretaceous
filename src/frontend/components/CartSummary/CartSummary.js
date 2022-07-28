3de206c556e1b859a81d899f48a5b91c0a035190
MarkVed17 <vedantlahane17@gmail.com>
2022-07-29 02:03:06 +0530

    refactor: updated `Cart Summary` (integrating razorpay)

--- src/frontend/components/CartSummary/CartSummary.js
+++ src/frontend/components/CartSummary/CartSummary.js
@@ -1,10 +1,58 @@
 import React from "react";
-import { useCart } from "../../contexts";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAuth, useCart, useUser } from "../../contexts";
+import { clearCartService, loadScript } from "../../services";
 import "./CartSummary.css";
 
 function CartSummary() {
-  const { cartSummary } = useCart();
+  const { cartSummary, setCart } = useCart();
+  const { deliveryAddress } = useUser();
+  const {
+    auth: { token },
+  } = useAuth();
+
   const { cartTotalProducts, cartTotalPrice, cartTotalAmount } = cartSummary;
+
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  async function displayRazorpay(e) {
+    e.preventDefault();
+    const response = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
+
+    if (!response) {
+      alert("Razorpay SDK failed to load. Are you online?");
+      return;
+    }
+
+    const options = {
+      key: process.env.REACT_APP_RAZORPAY_KEY_ID,
+      currency: "INR",
+      amount: cartTotalAmount * 100,
+      name: "Camp Cretaceous",
+      description: "Thank you for trusting us",
+      image: "",
+
+      handler: async (response) => {
+        const { razorpay_payment_id } = await response;
+        const orderData = {
+          orderAmount: cartTotalAmount,
+          razorpayId: razorpay_payment_id,
+        };
+        await clearCartService(setCart, token);
+        navigate("/products");
+      },
+      prefill: {
+        contact: "7777777777",
+        email: "vedantlahane@gmail.com",
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  }
 
   return (
     <div className="cart-summary">
@@ -38,9 +86,37 @@ function CartSummary() {
         </h2>
       </div>
       <hr />
-      <button className="btn btn-brand btn-accent checkout-btn">
-        Proceed to Checkout
-      </button>
+
+      {pathname === "/orderSummary" && (
+        <div className="address">
+          <h2>Address Details</h2>
+          <div className="address-details">
+            <p>{deliveryAddress?.name}</p>
+            <p>{deliveryAddress?.area}</p>
+            <p>{deliveryAddress?.locality}</p>
+            <p>
+              {deliveryAddress?.city} , {deliveryAddress?.pincode} ,
+              {deliveryAddress?.state}
+            </p>
+            <p>{deliveryAddress?.mobile}</p>
+          </div>
+        </div>
+      )}
+      {pathname === "/orderSummary" ? (
+        <button
+          className="btn btn-brand btn-accent checkout-btn"
+          onClick={displayRazorpay}
+        >
+          Place Order
+        </button>
+      ) : (
+        <button
+          className="btn btn-brand btn-accent checkout-btn"
+          onClick={() => navigate("/checkout")}
+        >
+          Proceed to Checkout
+        </button>
+      )}
     </div>
   );
 }
